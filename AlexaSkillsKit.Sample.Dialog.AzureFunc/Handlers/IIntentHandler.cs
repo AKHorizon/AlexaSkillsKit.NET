e2c604cefbfc192a55e72004594fbb393f4fbd1e
Anton Kuperman <akuperman@horizonsoftware.com>
2019-06-25 13:20:29 -0400

    Readded missing project files to attempt a pull request

---
+++ AlexaSkillsKit.Sample.Dialog.AzureFunc/Handlers/IIntentHandler.cs
@@ -0,0 +1,8 @@
+﻿using AlexaSkillsKit.Speechlet;
+using System.Threading.Tasks;
+
+namespace AlexaSkillsKit.Sample.Dialog.AzureFunc.Handlers {
+    public interface IIntentHandler {
+        Task<SpeechletResponse> OnIntentAsync(IntentRequest intentRequest, Session session);
+    }
+}
---
+++ AlexaSkillsKit.Sample/Speechlet/AlexaController.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2018 Stefan Negritoiu (FreeBusy) and contributors. See LICENSE file for more information.
+
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sample.Controllers
+{
+    public class AlexaController : ApiController
+    {
+        [Route("alexa/sample-session")]
+        [HttpPost]
+        public HttpResponseMessage SampleSession() {
+            var speechlet = new SampleSessionSpeechlet();
+            return speechlet.GetResponse(Request);
+        }
+    }
+}
